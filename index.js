ca93ecc851a5fdf9dc432dc2ba31ef062771112e
KwakuAldo <x.pac20032001@gmail.com>
2025-05-20 18:04:54 +0000

    add form validation and error messages

--- index.js
+++ index.js
@@ -8,23 +8,107 @@ document.querySelector(".btn").addEventListener("click", function () {
     this.classList.remove("bg-purple1");
     this.classList.add("bg-off-black");
 
-    let  currentDay = parseInt(document.querySelector("#day").value);
-    let  currentMonth = parseInt(document.querySelector("#month").value) - 1;
-    let  currentYear = parseInt(document.querySelector("#year").value);
+    //input elements
+    const dayInput = document.querySelector("#day");
+    const monthInput = document.querySelector("#month");
+    const yearInput = document.querySelector("#year");
 
 
+    // Get the values from the input fields
+    let  currentDay = parseInt(dayInput.value);
+    let  currentMonth = parseInt(monthInput.value) - 1;
+    let  currentYear = parseInt(yearInput.value);
+
+    // error message elements
+    const dayError = document.getElementById("day-error");
+    const monthError = document.getElementById("month-error");
+    const yearError = document.getElementById("error-year");
+
+    // label text color change on error
+    const labelText = document.querySelectorAll(".label-text")
+    function labelColorChangeWithError(labels, removeClass, addClass) {
+        labels.forEach((label) => {
+            label.classList.remove(removeClass);
+            label.classList.add(addClass);
+        });
+    }
+
+    // input element color change to alert error
+    const inputEl = document.querySelectorAll(".inputEl")
+    function inputElementColorChangeWithError(inputs, removeClass, addClass) {
+        inputs.forEach((inputs) => {
+            inputs.classList.remove(removeClass)
+            inputs.classList.add(addClass)
+        });
+    }
+
+    // CLear previous error messages
+    dayError.textContent = "";
+    monthError.textContent = "";
+    yearError.textContent = "";
+
+    // Validate the input values
+    let hasError = false;
+
+    // check if the input fields are empty
+    if (!dayInput.value) {
+        dayError.textContent = "This field is required";
+        labelColorChangeWithError(labelText, "text-smokey-grey", "text-light-red");
+        inputElementColorChangeWithError(inputEl, "border-smokey-grey", "border-light-red");
+        hasError = true;
+    }
+
+    if (!monthInput.value) {
+        monthError.textContent = "This field is required";
+        labelColorChangeWithError(labelText, "text-smokey-grey", "text-light-red");
+        inputElementColorChangeWithError(inputEl, "border-smokey-grey", "border-light-red");
+        hasError = true;
+    }
+    if (!yearInput.value) {
+        yearError.textContent = "This field is required";
+        labelColorChangeWithError(labelText, "text-smokey-grey", "text-light-red");
+        inputElementColorChangeWithError(inputEl, "border-smokey-grey", "border-light-red");
+        hasError = true;
+    }
+
+    // check range of the input values
+    if (!hasError) {
+        if (currentDay < 1 || currentDay > 31) {
+            dayError.textContent = "Must be a valid day";
+            hasError = true;
+        }
+        if (currentMonth < 0 || currentMonth > 11) {
+            monthError.textContent = "Must be a valid month";
+            hasError = true;
+        }
+        if (currentYear < 1900 || currentYear > new Date().getFullYear()) {
+            yearError.textContent = "Must be a valid year";
+            hasError = true;
+        }
+    }
+
+    // invalid input for month
     let birthDate = new Date(currentYear, currentMonth, currentDay);
+    if (
+        birthDate.getFullYear() !== currentYear ||
+        birthDate.getMonth() !== currentMonth ||
+        birthDate.getDate() !== currentDay
+    ) {
+        dayError.textContent = "Invalid date";
+        hasError = true;
+    }
+
+    if (birthDate > new Date()) {
+        yearError.textContent = "Must be in the past";
+        hasError = true;
+    }
+
+    if (hasError)
+        return;
+    
     let today = new Date();
 
-    if (isNaN(birthDate.getTime())) {
-        alert("Please enter a valid date");
-        return;
-    }
-    if (birthDate > today) {
-        alert("Please enter a date in the past");
-        return;
-    }
-    
+    // Calculate age
     let ageYears = today.getFullYear() - birthDate.getFullYear();
     let ageMonths = today.getMonth() - birthDate.getMonth();
     let ageDays = today.getDate() - birthDate.getDate();
